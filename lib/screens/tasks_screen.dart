ed3ea4f488fb705fa088ad06b9d796363ec28710
Parsa Hosseini <officer47p@gmail.com>
2019-10-05 09:39:28 +0330

    setup the basic design components

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -10,7 +10,41 @@ class _TasksState extends State<Tasks> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(),
+      body: SafeArea(child: Container(
+          padding: EdgeInsets.only(top: 20, left: 30, right: 30, bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 30,
+                child: Icon(
+                  Icons.list,
+                  color: Colors.lightBlueAccent,
+                  size: 30,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Todoey",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                "12 Tasks",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              )
+            ],
+          ),
+        ),),
     );
   }
 }
