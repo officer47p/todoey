7d0b5bfa1b50fbd3d5eb42fbfbe43608866d54bd
Parsa Hosseini <officer47p@gmail.com>
2019-10-05 10:06:11 +0330

    added the todos card

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -10,41 +10,64 @@ class _TasksState extends State<Tasks> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(child: Container(
-          padding: EdgeInsets.only(top: 20, left: 30, right: 30, bottom: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 30,
-                child: Icon(
-                  Icons.list,
-                  color: Colors.lightBlueAccent,
-                  size: 30,
-                ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding:
+                  EdgeInsets.only(top: 20, left: 30, right: 30, bottom: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.lightBlueAccent,
+                      size: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Todoey",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    "12 Tasks",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Todoey",
-                style: TextStyle(
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.w700,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
                 ),
               ),
-              Text(
-                "12 Tasks",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              )
-            ],
-          ),
-        ),),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
