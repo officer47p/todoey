6b16f3df242b1c1360f7fb3066be1b80a87cd53b
Parsa Hosseini <officer47p@gmail.com>
2019-10-05 23:08:10 +0330

    added checkbox functionality

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("Some Todo"),
-      trailing: Checkbox(
-        value: true,
+      title: Text(
+        "Some Todo",
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        isChecked: isChecked,
+        checkCallback: (newValue) {
+          setState(() {
+            isChecked = newValue;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({@required this.isChecked, @required this.checkCallback});
+  final Function checkCallback;
+  final bool isChecked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: checkCallback,
     );
   }
 }
